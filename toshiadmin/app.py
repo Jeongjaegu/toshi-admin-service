f5d5e34aa7c829aecbd843971953a4b648a8d4dc
Tristan King <tristan@bakkenbaeck.no>
2017-09-13 12:57:18 +0200

    add all users migration

--- toshiadmin/app.py
+++ toshiadmin/app.py
@@ -1075,6 +1075,7 @@ async def update_categories(request, current_user):
     to_env = request.form.get('to', None)
     toshi_ids = request.form.get('toshi_ids', None)
     apps = request.form.get('apps', None)
+    users = request.form.get('users', None)
 
     toshi_ids = set(re.findall("0x[a-fA-f0-9]{40}", toshi_ids))
 
@@ -1086,6 +1087,9 @@ async def update_categories(request, current_user):
             user_rows = list(users)
         else:
             user_rows = []
+        if users == 'on':
+            users = await con.fetch("SELECT * FROM users WHERE is_app = FALSE")
+            user_rows.extend(list(users))
         if len(toshi_ids) > 0:
             users = await con.fetch("SELECT * FROM users WHERE toshi_id = ANY($1)", toshi_ids)
             user_rows.extend(list(users))
