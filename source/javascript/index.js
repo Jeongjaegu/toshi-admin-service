930ebb8e5e0a3a6832c0bb564da377a3e7d80905
Megan Pearson <megan.e.pearson@gmail.com>
2018-03-02 17:18:22 +0100

    Implements inline editing for dapp table

--- source/javascript/index.js
+++ source/javascript/index.js
@@ -77,3 +77,85 @@ $("div.edit_category").dblclick((e) => {
   inp.focus();
   inp.select();
 });
+
+//edit dapp 
+$('tr.dapp').one('input change', function() {
+    var tr = $(this),
+        td = $('<td></td>'),
+        saveChangesButton = $('<button class="js-update" type="submit">Save Changes</button>'),
+        button = tr.find('.js-update');
+
+   if(button.length){
+       return;
+   } 
+
+    td.append(saveChangesButton);    
+    tr.append(td);
+
+    saveChangesButton.click(function(){
+        saveChangesButton.attr('disabled', true);
+    
+        var avatar = tr.find('.js-avatar')[0].files[0],
+            name = tr.find('.js-name').text(),
+            dappUrl = tr.find('.js-dapp-url').text(),
+            desc = tr.find('.js-desc').text(),
+            entityUrl = tr.data('entity-url'),
+            formData = new FormData();
+    
+        formData.append('avatar', avatar);
+        formData.append('name', name);
+        formData.append('url', dappUrl);
+        formData.append('description', desc);
+    
+        $.ajax({
+            type: 'POST',
+            url: entityUrl,
+            data: formData,
+            processData: false,
+            contentType:false,
+        }).done(function(success){
+        }).fail(function(jqXHR, textStatus, errorThrown){
+            console.log(jqXHR, textStatus, errorThrown);
+        }).always(function(){
+            saveChangesButton.remove();
+            saveChangesButton.attr('disabled', false);
+        });
+    });
+});
+
+//delete dapp
+$('.js-delete').click(function(){
+    var deleteButton = $(this),
+        tr = deleteButton.closest('tr'),
+        entityUrl = tr.data('entity-url');
+    
+    deleteButton.attr('disabled', true);
+
+    $.ajax({
+        type: 'POST',
+        url: entityUrl + '/delete'
+    }).done(function(success){
+        tr.remove();
+    }).fail(function(){
+        deleteButton.attr('disabled', false);
+    })
+});
+
+//preview the avatar
+function readURL(input) {
+    if (input.files && input.files[0]) {
+        var avatar = $(input).parent().find('img'),
+            reader = new FileReader();
+
+        reader.onload = function(e) {
+            avatar.attr('src', e.target.result);
+        }
+
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+
+//when new avatar uploaded, show new avatar
+$(".js-avatar").change(function() {
+    readURL(this);
+});
